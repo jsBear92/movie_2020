3da52e37785976bf2c7dcbdaef497461bf7a064c
js_bear <js104427@gmail.com>
2020-12-07 20:26:14 +0800

    Ch5 Add_Minus State Complete

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ class App extends React.Component {
         count: 0,
     };
 
+    add = () => {
+        this.setState(current => ({
+            count: current.count + 1,
+        }));
+    }
+
+    minus = () => {
+        this.setState(current => ({
+            count: current.count - 1,
+        }));
+    }
+
     render() {
-        return <h1>The number is: {this.state.count}</h1>;
+        return (
+            <div>
+                <h1>The number is: {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
     }
 }
 
